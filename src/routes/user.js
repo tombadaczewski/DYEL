8e31bc2f0e22fe75a5873c802c87572bf1d98046
Michael Corrigan <mcorrigan89@gmail.com>
2016-12-13 22:11:19 -0600

    couple more updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import models from '../models/index';
+import * as _ from 'lodash';
 
 let user = models.user;
 
@@ -8,6 +9,11 @@ router.get('/user', (req, res) => {
 
     user.findAll()
         .then((users) => {
+            let user = (n) => {
+                return { id: n.id, username: n.username };
+            };
+            users = _.map(users, user);
+            console.log(users)
             res.json({ users: users });
         })
         .catch((error) => {
@@ -17,7 +23,7 @@ router.get('/user', (req, res) => {
 
 router.get('/user/:id', (req, res) => {
 
-    user.findOne({ id: req.params.id })
+    user.findOne({where:{ id: req.params.id }})
         .then((user) => {
             res.json({ id: user.id, username: user.username });
         })
